63d923a666006fd4a486096a53820a7495687de0
jin ishida <jin.189321@gmail.com>
2025-06-27 08:22:45 +0900

    ハンバーガーメニューの調整

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("navToggle");
   const navList = document.getElementById("navList");
 
-  toggle.addEventListener("click", () => {
-    navList.classList.toggle("show");
-  });
+  if (toggle && navList) {
+    toggle.addEventListener("click", function () {
+      navList.classList.toggle("show");
+    });
+  }
 });
